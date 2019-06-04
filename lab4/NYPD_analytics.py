4f2d4ccc74f50f6aaca479f8885c608a64dbd349
Oleksandr Korzhenevskyi <aleksander3103@yandex.ua>
2019-06-04 14:38:13 +0300

    [lab4] The results for 3 prediction methods

--- lab4/NYPD_analytics.py
+++ lab4/NYPD_analytics.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import datetime
 from pathlib import Path
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARMA
 
 def WriteToFile( df, path ):
     file = open( path, 'w' )
@@ -17,6 +20,28 @@ def GetGroupedData(startDate, endDate):
     WriteToFile(df[:-10], 'without_last_10.csv')
     WriteToFile(df[-10:], 'last_10_ethalon.csv')
 
+def SES(base_df, steps):
+    data = base_df['Number'].tolist()
+    model = SimpleExpSmoothing(data)
+    model_fit = model.fit()
+    yhat = model_fit.predict(len(data), len(data) + steps - 1)
+    return np.round(yhat)
+
+def AutoRegression(base_df, steps):
+    data = base_df['Number'].tolist()
+    model = AR(data)
+    model_fit = model.fit()
+    yhat = model_fit.predict(len(data), len(data) + steps - 1)
+    return np.round(yhat)
+
+def AutoRegressionMA(base_df, steps):
+    data = base_df['Number'].tolist()
+    model = ARMA(data, order=(2, 1))
+    model_fit = model.fit(disp=False)
+    yhat = model_fit.predict(len(data), len(data) + steps - 1)
+    return np.round(yhat)
+
+
 startDate = datetime.datetime.strptime('2010-01-01', '%Y-%m-%d')
 endDate = datetime.datetime.strptime('2012-12-31', '%Y-%m-%d')
 
@@ -27,4 +52,17 @@ if not ( Path('without_last_10.csv').is_file() and Path('last_10_ethalon.csv').i
 base_df = pd.read_csv('without_last_10.csv')
 ethalon_df = pd.read_csv('last_10_ethalon.csv')
 
-print(ethalon_df)
+steps = 10
+ses_results = SES(base_df, steps)
+ar_results = AutoRegression(base_df, steps)
+arma_results = AutoRegressionMA(base_df, steps)
+
+result_df = ethalon_df.rename( index = str, columns = {'Number' : 'Ethalon' } )
+result_df['SES'] = ses_results
+result_df['SES_delta'] = np.abs(result_df['Ethalon'] - result_df['SES'])
+result_df['AR'] = ar_results
+result_df['AR_delta'] = np.abs(result_df['Ethalon'] - result_df['AR'])
+result_df['ARMA'] = arma_results
+result_df['ARMA_delta'] = np.abs(result_df['Ethalon'] - result_df['ARMA'])
+print (result_df.to_string())
+WriteToFile(result_df, 'results.csv')
