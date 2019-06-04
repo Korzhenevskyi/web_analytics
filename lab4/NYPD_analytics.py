74b59f1d20b2caf579f09dc4bad28e6210f2b412
Oleksandr Korzhenevskyi <aleksander3103@yandex.ua>
2019-06-04 12:39:13 +0300

    [lab4] Data is aggregated

--- lab4/NYPD_analytics.py
+++ lab4/NYPD_analytics.py
@@ -2,19 +2,29 @@ __author__ = 'Oleksandr Korzhenevskyi'
 import pandas as pd
 import numpy as np
 import datetime
+from pathlib import Path
 
-df = pd.read_csv('NYPD_Complaint_Data_Historic.csv', usecols=['CMPLNT_FR_DT'])
-df['CMPLNT_FR_DT'] = pd.to_datetime(df['CMPLNT_FR_DT'], infer_datetime_format=True, errors='coerce')
-df['CMPLNT_FR_DT'] = df.dropna()
+def WriteToFile( df, path ):
+    file = open( path, 'w' )
+    file.write( df.to_csv( index = False ) )
+    file.close()
+
+def GetGroupedData(startDate, endDate):
+    df = pd.read_csv('NYPD_Complaint_Data_Historic.csv', usecols=['CMPLNT_FR_DT'])
+    df['CMPLNT_FR_DT'] = pd.to_datetime(df['CMPLNT_FR_DT'], infer_datetime_format=True, errors='coerce')
+    df['CMPLNT_FR_DT'] = df.dropna()
+    df = df[(df['CMPLNT_FR_DT'] >= startDate) & (df['CMPLNT_FR_DT'] <= endDate)].groupby(['CMPLNT_FR_DT']).size().reset_index(name='Number')
+    WriteToFile(df[:-10], 'without_last_10.csv')
+    WriteToFile(df[-10:], 'last_10_ethalon.csv')
+
 startDate = datetime.datetime.strptime('2010-01-01', '%Y-%m-%d')
-# endDate = '2012-12-31'
 endDate = datetime.datetime.strptime('2012-12-31', '%Y-%m-%d')
-# endDate = '12/13/2012'
-startYear = '2010'
-endYear = '2012'
 
-df = df[(df['CMPLNT_FR_DT'] >= startDate) & (df['CMPLNT_FR_DT'] <= endDate)]
-#
 
-# print (df.to_string())
-print(df)
+if not ( Path('without_last_10.csv').is_file() and Path('last_10_ethalon.csv').is_file() ):
+    GetGroupedData(startDate, endDate)
+
+base_df = pd.read_csv('without_last_10.csv')
+ethalon_df = pd.read_csv('last_10_ethalon.csv')
+
+print(ethalon_df)
